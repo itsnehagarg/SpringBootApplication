780939bbc28a4851b9df459fb979597b1d21b401
itsnehagarg <nehagarg.94@gmail.com>
2023-02-25 22:43:42 -0600

    Initial Commit

---
+++ src/main/java/com/example/NewProjectSpringBoot/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.NewProjectSpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello from Spring Boot!";
+	}
+
+}
